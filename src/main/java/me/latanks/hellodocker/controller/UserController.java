07fc08c8dd3fcd90416cf9f1bc1c7fc76e371e36
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2025-02-03 13:39:50 -0300

    Remove dependece MYSql & JPQ

--- src/main/java/me/latanks/hellodocker/controller/UserController.java
+++ src/main/java/me/latanks/hellodocker/controller/UserController.java
@@ -1,39 +1,12 @@
 package me.latanks.hellodocker.controller;
 
-import me.latanks.hellodocker.model.User;
-import me.latanks.hellodocker.repository.IUserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-
-    @Autowired
-    private IUserRepository userRepository;
-
     @GetMapping("/hello")
     public String hello() {
         return "Hello Docker!";
-    }
-
-    @GetMapping
-    public Iterable<User> getUsers() {
-        return userRepository.findAll();
-    }
-
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
-        return userRepository.findById(id).orElse(null);
-    }
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        return userRepository.save(user);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
-        userRepository.deleteById(id);
     }
 }
--- src/main/java/me/latanks/hellodocker/repository/IUserRepository.java
+++
@@ -1,8 +0,0 @@
-package me.latanks.hellodocker.repository;
-
-
-import me.latanks.hellodocker.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepository extends JpaRepository<User, Long> {
-}
